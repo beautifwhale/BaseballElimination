a85babfd87ee6c755906a0fcfc6630d322041871
David Morton <davidmortonmail@gmail.com>
2014-12-05 14:47:21 +0000

    added .jar files for project and created main class file

---
+++ src/BaseballElimination.java
@@ -0,0 +1,31 @@
+
+public class BaseballElimination {
+	
+	public BaseballElimination()
+	{
+		
+	}
+	
+	public int numberOfTeams()
+	{
+		return 0;
+	}
+	
+	public Iterable<String> teams()
+	{
+		return null;
+		
+	}
+	
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
