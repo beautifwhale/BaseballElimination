fb18898902a5c38f3a0dadd97bafba2eb660c0cc
David Morton <davidmortonmail@gmail.com>
2014-12-09 14:59:17 +0000

    added edge from 0 to 1 in flow network

--- src/BaseballElimination.java
+++ src/BaseballElimination.java
@@ -78,7 +78,9 @@ public class BaseballElimination {
 		}
 		// create a flow network
 		// number of vertices = s + (Number of teams * (Number of teams -1)) + t
-		FlowNetwork m_FlowNetwork = new FlowNetwork(1 + (m_iNumberOfTeams * (m_iNumberOfTeams -1)) + 1);
+		FlowNetwork m_FlowNetwork = new FlowNetwork(m_iNumberOfTeams * (m_iNumberOfTeams - 1));
+		StdOut.println(m_FlowNetwork);
+		m_FlowNetwork.addEdge(new FlowEdge(0, 1, Double.POSITIVE_INFINITY));
 		StdOut.println(m_FlowNetwork);
 		
 	}
